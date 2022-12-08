419fd5b62af7e8cbc1aac94e6bf581abe950decc
Matthew Johnson <matthewjohnson@snarkydata.com>
2022-12-08 06:03:17 -0600

    Adding Listing-11-26

---
+++ Listing-11-26/mod_str.c
@@ -0,0 +1,48 @@
+// mod_str.c -- modifies a string
+
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define LIMIT 80
+
+void ToUpper(char *);
+int PunctCount(const char *);
+
+int main(void) {
+
+	char line[LIMIT];
+
+	puts("Please enter a line: ");
+	fgets(line, LIMIT, stdin); 	// replace gets() with fgets()
+	line[strcspn(line, "\n")] = 0; 	// added to buffer '\n'
+
+	ToUpper(line);
+	puts(line);
+	printf("That line has %d punctuation characters.\n", 
+		PunctCount(line));
+
+	return 0;
+}
+
+void ToUpper(char * str) {
+
+	while (*str) {
+
+		*str = toupper(*str);
+		str++;
+	}
+}
+
+int PunctCount(const char * str) {
+
+	int ct = 0;
+	while (*str) {
+
+		if (ispunct(*str))
+			ct++;
+		str++;
+	}
+
+	return ct;
+}
